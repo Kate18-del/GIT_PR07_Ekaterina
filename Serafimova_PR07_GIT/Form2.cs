3274851c82bfce5154dc1a5fbf6026c323e3c6c3
Serafimova <kassarety1980@gmail.com>
2026-01-13 17:16:00 +0300

    Логика для кнопки

--- Serafimova_PR07_GIT/Form2.cs
+++ Serafimova_PR07_GIT/Form2.cs
@@ -16,5 +16,10 @@ namespace Serafimova_PR07_GIT
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Привет, Серафимова Екатерина Сергеевна");
+        }
     }
 }
